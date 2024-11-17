678d4ae5c189f7867b85aeee8f432dae040180cb
PigeonsHouse <bullbull1230@gmail.com>
2024-11-17 16:13:20 +0900

    First Commit
    Co-authored-by: OTSUKA Shintaro <otsuka.shintaro600@mail.kyutech.jp>

---
+++ vsml_core/src/schemas.rs
@@ -0,0 +1,194 @@
+use std::collections::HashMap;
+use std::fmt;
+use std::fmt::{Debug, Formatter};
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[derive(Debug, Clone)]
+pub struct StyleData {
+    pub layer_mode: Option<LayerMode>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum LayerMode {
+    Multi,
+    Single,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum LayerModeParseError {
+    UnknownMode,
+}
+
+impl FromStr for LayerMode {
+    type Err = LayerModeParseError;
+
+    fn from_str(value: &str) -> Result<LayerMode, Self::Err> {
+        match value {
+            "multi" => Ok(LayerMode::Multi),
+            "single" => Ok(LayerMode::Single),
+            _ => Err(LayerModeParseError::UnknownMode),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum TimeUnit {
+    Percent,
+    Frame,
+    Second,
+    Fit,
+}
+#[derive(Clone, Debug, PartialEq)]
+pub enum Duration {
+    Percent(f64),
+    Frame(usize),
+    Second(f64),
+    Fit,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum DurationParseError {
+    NumberParseError,
+    UnknownUnit,
+}
+
+impl FromStr for Duration {
+    type Err = DurationParseError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        if value == "fit" {
+            Ok(Duration::Fit)
+        } else if value == "0" {
+            Ok(Duration::Frame(0))
+        } else if let Some(value) = value.strip_suffix('s') {
+            let val = value
+                .parse()
+                .map_err(|_| DurationParseError::NumberParseError)?;
+            Ok(Duration::Second(val))
+        } else if let Some(value) = value.strip_suffix('f') {
+            let val = value
+                .parse()
+                .map_err(|_| DurationParseError::NumberParseError)?;
+            Ok(Duration::Frame(val))
+        } else if let Some(value) = value.strip_suffix('%') {
+            let val = value
+                .parse()
+                .map_err(|_| DurationParseError::NumberParseError)?;
+            Ok(Duration::Percent(val))
+        } else {
+            Err(DurationParseError::UnknownUnit)
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Order {
+    Sequence,
+    Parallel,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum OrderParseError {
+    UnknownMode,
+}
+impl FromStr for Order {
+    type Err = OrderParseError;
+
+    fn from_str(value: &str) -> Result<Order, Self::Err> {
+        match value {
+            "sequence" => Ok(Order::Sequence),
+            "parallel" => Ok(Order::Parallel),
+            _ => Err(OrderParseError::UnknownMode),
+        }
+    }
+}
+
+pub enum ObjectType {
+    Seq,
+    Prl,
+    Other(Arc<dyn ObjectProcessor>),
+}
+
+impl Debug for ObjectType {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            ObjectType::Seq => write!(f, "Seq"),
+            ObjectType::Prl => write!(f, "Prl"),
+            ObjectType::Other(p) => write!(f, "Other({})", p.name()),
+        }
+    }
+}
+
+pub trait ObjectProcessor {
+    fn name(&self) -> &str;
+    fn default_duration(&self, attr: ()) -> f64;
+    // fn process(&self, attr: /* ? */, children: /* ? */, at: /* Time */) -> (/* ? */);
+}
+
+#[derive(Debug)]
+pub enum ObjectData {
+    Element {
+        object_type: ObjectType,
+        start_time: f64,
+        attributes: HashMap<String, String>,
+        order: Option<Order>,
+        duration: Option<Duration>,
+        styles: StyleData,
+        children: Vec<ObjectData>,
+    },
+    Text(String),
+}
+
+#[derive(Debug)]
+pub struct IVData {
+    pub resolution_x: usize,
+    pub resolution_y: usize,
+    pub fps: f64,
+    pub sampling: usize,
+    pub objects: Vec<ObjectData>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_layer_mode() {
+        assert_eq!("multi".parse::<LayerMode>(), Ok(LayerMode::Multi));
+        assert_eq!("single".parse::<LayerMode>(), Ok(LayerMode::Single));
+        assert_eq!(
+            "unknown".parse::<LayerMode>(),
+            Err(LayerModeParseError::UnknownMode)
+        );
+    }
+
+    #[test]
+    fn test_parse_duration() {
+        assert_eq!("fit".parse::<Duration>(), Ok(Duration::Fit));
+        assert_eq!("0".parse::<Duration>(), Ok(Duration::Frame(0)));
+        assert_eq!("1f".parse::<Duration>(), Ok(Duration::Frame(1)));
+        assert_eq!("1s".parse::<Duration>(), Ok(Duration::Second(1.0)));
+        assert_eq!("1.0s".parse::<Duration>(), Ok(Duration::Second(1.0)));
+        assert_eq!("1%".parse::<Duration>(), Ok(Duration::Percent(1.0)));
+        assert_eq!("1.0%".parse::<Duration>(), Ok(Duration::Percent(1.0)));
+        assert_eq!(
+            "1.0".parse::<Duration>(),
+            Err(DurationParseError::UnknownUnit)
+        );
+        assert_eq!(
+            "1".parse::<Duration>(),
+            Err(DurationParseError::UnknownUnit)
+        );
+    }
+
+    #[test]
+    fn test_parse_order() {
+        assert_eq!("sequence".parse::<Order>(), Ok(Order::Sequence));
+        assert_eq!("parallel".parse::<Order>(), Ok(Order::Parallel));
+        assert_eq!(
+            "unknown".parse::<Order>(),
+            Err(OrderParseError::UnknownMode)
+        );
+    }
+}
