df35dbf84386d77e65f1b7bc36d6e64824652d29
PigeonsHouse <bullbull1230@gmail.com>
2024-12-06 21:40:10 +0900

    wip

--- vsml_core/src/lib.rs
+++ vsml_core/src/lib.rs
@@ -6,10 +6,10 @@ pub mod schemas;
 
 #[derive(Debug)]
 pub struct RenderObject {
-    width: f64,
-    height: f64,
-    x: f64,
-    y: f64,
+    width: f32,
+    height: f32,
+    x: f32,
+    y: f32,
     src: Arc<dyn ObjectProcessor>,
     attributes: HashMap<String, String>,
     children: Vec<RenderObject>,
@@ -26,49 +26,76 @@ pub struct RenderData {
 pub struct MixData {}
 
 pub struct Rect {
-    x: f64,
-    y: f64,
-    width: f64,
-    height: f64,
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
 }
 
-struct TextStyleData {}
+struct TextStyleData {
+    color: String,
+    font_name: String,
+}
 
 struct TextData {
     text: String,
     style: TextStyleData,
 }
 
+struct Property {
+}
+
+struct EffectStyle {
+}
+
+struct RenderingInfo {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+}
+struct TextRenderingInfo {
+    x: f32,
+    y: f32,
+    max_width: f32,
+    max_height: f32,
+}
+
 pub trait Renderer {
     type Image;
-    fn render_image(&mut self, x, y, width, height, Self::Image);
-    fn render_text(&mut self, x, y, max_width, max_height, text_data: &[TextData]) -> Rect;
-    fn render_box(&mut self, x, y, width, height, (/* Style */));
+    fn render_image(&mut self, image: Self::Image, info: RenderingInfo);
+    fn render_text(&mut self, text_data: &[TextData], info: TextRenderingInfo) -> Rect;
+    fn render_box(&mut self, property: Property, info: RenderingInfo);
     fn render(self, width: u32, height: u32) -> Self::Image;
 }
 
-pub trait RendererCreator {
-    type Renderer: Renderer;
+pub trait ImageStyleApplier {
+    type Image;
+    fn apply_style(&mut self, image: Self::Image, style: EffectStyle) -> Self::Image;
+}
+
+pub trait RendererFactory {
+    type Image;
+    type Renderer: Renderer<Image=Self::Image>;
     fn create_renderer(&mut self) -> Self::Renderer;
-    fn apply_style(&mut self, <Self::Renderer as Renderer>::Image, (/* Style */)) -> <Self::Renderer as Renderer>::Image;
 }
 
 fn convert_to_render_info<R>(
     iv_data: &schemas::IVData<<R::Renderer as Renderer>::Image>,
     frame_number: usize,
-    mut renderer_creator: R,
+    mut rendering_context: R,
 ) -> <R::Renderer as Renderer>::Image
 where
-    R: RendererCreator,
+    R: RendererFactory<Image=<R as ImageStyleApplier>::Image> + ImageStyleApplier,
 {
-    let mut renderer0 = renderer_creator.create_renderer();
-    let text_rect =renderer0.render_text();
-        let mut renderer = renderer_creator.create_renderer();
-        // render.render_text();
-        renderer.render_image();
-        let child_image = renderer.render(200, 200);
-        let image = object_processor.process(child_image, attributes);
-    renderer0.render_image(image);
+    // let mut renderer0 = rendering_context.create_renderer();
+    // let text_rect =renderer0.render_text();
+    //     let mut renderer = rendering_context.create_renderer();
+    //     render.render_text();
+    //     renderer.render_image();
+    //     let child_image = renderer.render(200, 200);
+    //     let image = object_processor.process(child_image, attributes);
+    // renderer0.render_image(image);
     todo!()
 }
 
