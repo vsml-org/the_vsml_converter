b0ca4b031d123f188b0830da77c6b61fb9d3acb7
PigeonsHouse <bullbull1230@gmail.com>
2025-01-15 23:46:24 +0900

    update: 動画時間が0秒のVSMLもassertで落ちるよう修正

--- vsml_encoder/src/lib.rs
+++ vsml_encoder/src/lib.rs
@@ -19,6 +19,7 @@ pub fn encode<R, M>(
     let ObjectData::Element { duration, .. } = iv_data.object else {
         panic!()
     };
+    assert_ne!(duration, 0.0, "動画時間が0秒になっています");
     assert!(duration.is_finite(), "動画時間が無限になっています");
     let whole_frames = duration * iv_data.fps as f64;
 
