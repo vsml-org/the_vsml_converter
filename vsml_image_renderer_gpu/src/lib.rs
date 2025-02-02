06a09649d68336c729cd8a17bdbdd35cbb78f007
PigeonsHouse <bullbull1230@gmail.com>
2025-02-03 01:16:08 +0900

    wip: GPUでのrendererの実装

--- vsml_image_renderer_gpu/src/lib.rs
+++ vsml_image_renderer_gpu/src/lib.rs
@@ -1,17 +1,31 @@
+#[cfg(test)]
+mod tests;
+
 use vsml_core::{ImageEffectStyle, Property, Rect, Renderer, RenderingContext, RenderingInfo, TextData, TextRenderingInfo};
 use vsml_common_image::Image as VsmlImage;
 
 pub struct RendererImpl {
     images: Vec<(VsmlImage, RenderingInfo)>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    render_pipeline: wgpu::RenderPipeline,
+    texture_bind_group_layout: wgpu::BindGroupLayout,
+    sampler: wgpu::Sampler,
 }
 
-pub struct RenderingContextImpl {}
+pub struct RenderingContextImpl {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    render_pipeline: wgpu::RenderPipeline,
+    texture_bind_group_layout: wgpu::BindGroupLayout,
+    sampler: wgpu::Sampler,
+}
 
 impl Renderer for RendererImpl {
     type Image = VsmlImage;
 
     fn render_image(&mut self, image: Self::Image, info: RenderingInfo) {
-        todo!()
+        self.images.push((image, info));
     }
 
     fn render_text(&mut self, text_data: &[TextData], info: TextRenderingInfo) -> Rect {
@@ -23,7 +37,68 @@ impl Renderer for RendererImpl {
     }
 
     fn render(self, width: u32, height: u32) -> Self::Image {
-        todo!()
+        let texture = self.device.create_texture(
+            &wgpu::TextureDescriptor {
+                label: None,
+                size: wgpu::Extent3d {
+                    width,
+                    height,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::COPY_SRC,
+                view_formats: &[],
+            }
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self.device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor { label: None }
+        );
+
+        self.images.iter().for_each(|(image, info)| {
+            let child_view = image.create_view(&wgpu::TextureViewDescriptor::default());
+
+            let diffuse_bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
+                layout: &self.texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&child_view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&self.sampler),
+                    },
+                ],
+                label: None,
+            });
+
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Load,
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+            render_pass.set_pipeline(&self.render_pipeline); // 2.
+            render_pass.set_bind_group(0, &diffuse_bind_group, &[]); // 1.
+            render_pass.set_scissor_rect(0, 0, width, height);
+            render_pass.set_viewport(info.x, info.y, info.width, info.height, 0.0, 1.0);
+            render_pass.draw(0..3,  0..1); // 3
+        });
+        self.queue.submit(std::iter::once(encoder.finish()));
+        texture
     }
 }
 
@@ -32,10 +107,116 @@ impl RenderingContext for RenderingContextImpl {
     type Renderer = RendererImpl;
 
     fn create_renderer(&mut self) -> Self::Renderer {
-        todo!()
+        RendererImpl {
+            images: vec![],
+            device: self.device.clone(),
+            queue: self.queue.clone(),
+            render_pipeline: self.render_pipeline.clone(),
+            texture_bind_group_layout: self.texture_bind_group_layout.clone(),
+            sampler: self.sampler.clone(),
+        }
     }
 
     fn apply_style(&mut self, image: Self::Image, style: ImageEffectStyle) -> Self::Image {
         todo!()
     }
 }
+
+impl RenderingContextImpl {
+    pub fn new(device: wgpu::Device, queue: wgpu::Queue) -> Self {
+        let texture_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+                label: Some("texture_bind_group_layout"),
+            });
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("render_image.wgsl").into()),
+        });
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[
+                    &texture_bind_group_layout,
+                ],
+                push_constant_ranges: &[],
+            });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[
+
+                ],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: None,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+            cache: None,
+        });
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
+        Self {
+            device,
+            queue,
+            render_pipeline,
+            texture_bind_group_layout,
+            sampler,
+        }
+    }
+}
+
