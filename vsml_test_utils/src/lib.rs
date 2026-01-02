a367091d1bfa3b9e6f74c3054e331d5613ff1c34
White-Green <43771790+White-Green@users.noreply.github.com>
2026-01-02 10:36:02 +0900

    refactor(vrt_out_path): improve test name validation and simplify path construction

--- vsml_test_utils/src/lib.rs
+++ vsml_test_utils/src/lib.rs
@@ -6,23 +6,22 @@ macro_rules! vrt_out_path {
         use std::path::PathBuf;
         use std::thread;
 
-        let test_name = thread::current().name().unwrap_or("unknown").to_string();
-        if !test_name.contains("vrt") {
-            panic!(
+        let current_thread = thread::current();
+        let test_name = match current_thread.name() {
+            Some(test_name) if test_name.contains("vrt") => test_name,
+            test_name => panic!(
                 "Test name must contain 'vrt' to use vrt_out_path macro. Current test name: {}",
-                test_name
-            );
-        }
+                test_name.unwrap_or("unknown")
+            ),
+        };
 
         let path = if let Ok(vrt_root) = env::var("VSML_VRT_OUTPUT_PATH") {
             let mut p = PathBuf::from(vrt_root);
             p.push(env!("CARGO_CRATE_NAME"));
-            p.push(test_name.replace("::", "/"));
+            p.extend(test_name.split("::"));
             p.push($file_name);
 
-            if let Some(parent) = p.parent() {
-                fs::create_dir_all(parent).expect("Failed to create VRT output directory");
-            }
+            fs::create_dir_all(p.parent().unwrap()).expect("Failed to create VRT output directory");
             p
         } else {
             PathBuf::from($file_name)
